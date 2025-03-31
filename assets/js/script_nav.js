17c88eee89c107b7d83be0fc377c64f37803fb71
Nickgiresse <kamdemnick12@gmail.com>
2025-03-31 23:15:00 +0200

    page index

--- assets/js/script_nav.js
+++ assets/js/script_nav.js
@@ -5,15 +5,22 @@ const boutonremoveSidebar=document.getElementById('removeSidebar')
 const sidebar=document.getElementById("sidebar")
 window.addEventListener("scroll",()=>{
     if (window.scrollY>100) {
-        console.log("dsfsfd");
+      
+        sidebar.style.top = 0
         nav.style.position="fixed"
         nav.style.top = 0
         nav.style.height= 60+"px"
         nav.style.transition=.2+"s"
+        sidebar.style.position="fixed"
+        sidebar.style.top = 0
+
+
     }else{
+       
         nav.style.position="relative"
         nav.style.transition=.2+"s"
         nav.style.height= 48+"px"
+       
 
     }
      
